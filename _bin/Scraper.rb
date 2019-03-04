be9bfead9483ea43aa909b98b8260f32c976f58d
Austin Perez <austin@austinp.com>
2019-03-04 11:41:02 -0800

    Adds ability to retrieve: Name, Price, Shipping Cost, Product Specs, Product Options, and Product Option Images from AliExpress

--- _bin/Scraper.rb
+++ _bin/Scraper.rb
@@ -7,7 +7,6 @@ require 'pry'
 require 'active_support'
 require 'active_support/core_ext'
 require_relative 'Looper'
-#require 'Open_uri'
 
 
 class Scraper
@@ -18,10 +17,24 @@ class Scraper
 		@parse_page ||= Nokogiri::HTML(doc)
 	end
 
-  def download_image(url, dest)
+  def download_image(url, file_name)
     open(url) do |u|
-      File.open(dest, 'wb') { |f| f.write(u.read) }
+      File.open(File.join(File.expand_path("..", Dir.pwd), "/_assets/images", file_name), 'wb') { |f| f.write(u.read) }
     end
+  end
+
+  def new_file_name(file, index)
+    file, extension = file.split('.')
+    index = sprintf '%02d', index
+
+    return file + '-' + index.to_s + '.' + extension
+  end
+
+  def get_pro_res_file(url)
+    path = url.gsub(url.split('/').last, "")
+    file = url.split('/').last.split('_').first
+    
+    return path + file
   end
 
 	def get_name
@@ -47,29 +60,40 @@ class Scraper
   def get_shipping(url)
     browser = Watir::Browser.new
     browser.goto(url)
-    browser.element(css: ".logistics-cost").wait_until_present
-    js_rendered_content = browser.element(css: ".logistics-cost")
-
-    js_rendered_content.text
+    browser.element(css: ".logistics-cost").wait_until
+    js_rendered_content = browser.element(css: ".logistics-cost").text
     browser.close
+
+    return js_rendered_content
   end
 
   def get_options
     option_list = parse_page.css("#j-product-info-sku").children
     list ||= []
-    children_list ||= []
 
     option_list.each do |item|
       next if !item.text.gsub(/\s+/, '').present?
 
-      list << item.text.gsub(/\s+/, '').split(':')[0]
+      option_name = item.text.gsub(/\s+/, '').split(':')[0]
+      children_list ||= []
       
-      item.css('li a').each do |child|
-        children_list << { title: child.try { |p| p["title"] }, image: child.css("img").first.try { |p| p["src"] } }
-      end
-    end
+      item.css('li a').each_with_index do |child, index|
 
-    list.concat(children_list)
+        if child.css("img").present?
+          title = child.try { |p| p["title"] }
+          image = child.css("img").first.try { |p| p["src"] }
+          image_file_name = image.split('/').last.split('_').first
+
+          children_list << { title: title, image: image }
+
+          download_image(get_pro_res_file(image), new_file_name(image_file_name, index))
+        else
+          children_list << { title: child.children.try { |p| p.first.text } }
+        end
+      end
+      list << { option_name => children_list } 
+    end
+    return list
   end
 
   def get_specifications
@@ -93,10 +117,6 @@ class Scraper
   # end
 
   private
-
-  # def item_container
-  # 	parse_page.css(".grid-item-info")
-  # end
 
   looper = Looper.new
 
